92010a607ff4e12b1ffdcbd8daa3963845a9f44e
oungo <y2k6273@naver.com>
2022-07-25 21:43:00 +0900

    Micro-Frontend demo

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
+import Header from 'home/Header'
+import Footer from 'home/Footer'
+
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: pdp</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+    <Header />
+    <div className="my-10">
+      PDP Page Content
+    </div>
+    <Footer />
   </div>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
