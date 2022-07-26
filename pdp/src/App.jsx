096750d6b081ae3d49a63bfa9b81104c8a5351e9
oungo <y2k6273@naver.com>
2022-07-26 22:57:00 +0900

    Error Handling

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 
 import "./index.scss";
 
+import SafeCompnent from './SafeComponent';
 import Header from 'home/Header'
 import Footer from 'home/Footer'
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header />
+    <SafeCompnent>
+      <Header />
+    </SafeCompnent>
     <div className="my-10">
       PDP Page Content
     </div>
