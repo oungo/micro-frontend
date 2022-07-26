7e13345471af075bc71ca544f44c645d92876793
oungo <y2k6273@naver.com>
2022-07-26 22:46:08 +0900

    Asynchronous Loading

--- pdp/src/App.jsx
+++ pdp/src/App.jsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./index.scss";
 
-import Header from 'home/Header'
+const Header = React.lazy(() => import("home/Header"));
 import Footer from 'home/Footer'
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <Header />
-    <div className="my-10">
-      PDP Page Content
+const App = () => {
+  const [showHeader, setShowHeader] = useState(false);
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      {showHeader && 
+        <Suspense fallback={<div>Loading</div>}>
+          <Header />
+        </Suspense>
+      }
+      <button className="text-3xl p-5"
+      onClick={() => setShowHeader(true)}>Show The Header</button>
+      <div className="my-10">
+        PDP Page Content
+      </div>
+      <Footer /> 
     </div>
-    <Footer />
-  </div>
-);
+  )
+  
+  };
 ReactDOM.render(<App />, document.getElementById("app"));
