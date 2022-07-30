847f13f8165f17c80be26496ec5b635d7478bb06
oungo <y2k6273@naver.com>
2022-07-30 15:43:54 +0900

    add HomeContent

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -5,12 +5,13 @@ import "./index.scss";
 
 import Header from './Header'
 import Footer from './Footer'
+import HomeContent from './HomeContent'
 
 const App = () => (
   <div className="mt-10 text-3xl mx-auto max-w-6xl">
     <Header />
     <div className="my-10">
-      Home Page Con tent
+      <HomeContent />
     </div>
     <Footer />
   </div>
