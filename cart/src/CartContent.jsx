fd4b98060ddaf81d93cb12ba9d9e6682ed9d9ae9
oungo <y2k6273@naver.com>
2022-08-15 20:44:20 +0900

    Finishing the Cart

--- cart/src/CartContent.jsx
+++ cart/src/CartContent.jsx
@@ -1,20 +1,58 @@
-import React, { useState, useEffect } from "react";
-import MiniCart from "./MiniCart";
-import { jwt } from './cart';
-import Login from './Login';
+import React, { useState, useEffect, Fragment  } from "react";
+import { cart, clearCart } from 'cart/cart';
+import { currency } from 'home/products';
 
 export default function CartContent() { 
-  const [token, setToken] = useState("");
+  const [items, setItems] = useState([] )
 
-  useEffect(() => {
-    return jwt.subscribe((val) => setToken(val ?? ''))
-  }, []);
+  useEffect(
+    () => cart.subscribe((value) => setItems(value?.cartItems ?? [])), []
+  );
   
   return (
-    <div>
-      <div>JWT: {token}</div>
-      <Login />
-      <MiniCart />
-    </div>
+    <>
+      <div className="my-10 grid grid-cols-4 gap-5">
+        {items.map((item) => (
+          <Fragment key={item.id}>
+            <div>{item.quantity}</div>
+            <img src={item.image} alt={item.name} className="max-h-6" />
+            <div>{item.name}</div>
+            <div className='text-right'>
+              {currency.format(item.quantity * item.price)}
+            </div> 
+          </Fragment>
+        ))}
+        <div></div>
+        <div></div>
+        <div></div>
+        <div className="text-right">
+          {currency.format(
+            items.reduce((a,v) => a + v.quantity * v.price, 0)
+          )}
+        </div>
+      </div>
+      {items.length > 0 && (
+        <div className="flex mb-10">
+          <div className='flex-grow'>
+            <button 
+              id="clearcart" 
+              className="bg-white border border-green-800 text-green-800 py-2 px-5 text-sm"
+              onClick={clearCart}
+            >
+              Clear Cart 
+            </button>
+          </div>
+          <div className='flex-end'>
+            <button 
+              id="clearcart" 
+              className="bg-green-800 border border-green-800 text-white py-2 px-5 text-sm"
+              onClick={clearCart}
+            >
+              Checkout
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
